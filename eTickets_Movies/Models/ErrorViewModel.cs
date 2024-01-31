c3f940ceb97c514ee74e9e016650dcc18c2cb351
Latifat Adewuyi <latifatadewuyi@gmail.com>
2024-01-31 23:36:18 +0100

    Add project files.

---
+++ eTickets_Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTickets_Movies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
